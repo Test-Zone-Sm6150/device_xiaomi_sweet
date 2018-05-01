6f6acefc5fa7edf4b133dca5d1fa58098e56676f
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    sweet: overlay: Enable option for full screen aspect ratio
    Change-Id: I2a1b2acc5ed97ccb6795dc371c2b3ac9cb4f9d03

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
