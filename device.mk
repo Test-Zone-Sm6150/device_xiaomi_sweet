bf7d155e1400e7606fd55701573ff8ee0e7d9bd5
AbhiShek Aggarwal <warabhishek@gmail.com>
2021-07-10 12:04:25 +0530

    sweet: overlay: Import brightness overlays from MIUI
    Change-Id: Ib00b3817eece7bb8ff0acafb3d545e4324d7c26d

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
