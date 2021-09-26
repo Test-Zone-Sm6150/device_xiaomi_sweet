a5522ade1a4210e009fdf24f9e3145bb3383ba25
Niranjan BR <niranjankannan2003@gmail.com>
2021-09-27 02:18:47 +0530

    sweet: Define kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,25 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 TARGET_BOOTLOADER_BOARD_NAME := sweet
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+# Kernel
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := vendor/sweet_user_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
+
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
 
